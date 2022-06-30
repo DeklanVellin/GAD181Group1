4a5683ff035d6c6f18b2983185004ee97c031905
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-06-30 21:42:06 +1000

    Connected Sword Assembly to transition screens

--- Assets/Scripts/SwordAssembly/SwordAssembly_Controller.cs
+++ Assets/Scripts/SwordAssembly/SwordAssembly_Controller.cs
@@ -9,27 +9,37 @@ public class SwordAssembly_Controller : MonoBehaviour
 
     float gameTimer = 4;
 
+    SceneController gameComplete;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameComplete = FindObjectOfType<SceneController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Count down 4 seconds for player to complete the game
         gameTimer -= Time.deltaTime;
 
         if(sequenceProgress == 5 && gameActive)
         {
+            // The player wins if they complete the sequence within the time
+            // Then notify the transition manager the game has been won
             print("YOU WIN!!!");
             gameActive = false;
-        }
 
-        if(gameTimer <= 0 && gameActive)
+            gameComplete.WinScreen(true);
+        }
+        else if(gameTimer <= 0 && gameActive)
         {
+            // The player loses if time runs out
+            // Notify the transition manager the game has been lost
             print("You Lost :(");
             gameActive = false;
+
+            gameComplete.WinScreen(false);
         }
     }
 
@@ -43,8 +53,12 @@ public class SwordAssembly_Controller : MonoBehaviour
             }
             else
             {
+                // The player loses if they press an incorrect button
+                // Notify the transition manager the game has been lost
                 print("You Lose :(");
                 gameActive = false;
+
+                gameComplete.WinScreen(false);
             }
         }
     }
