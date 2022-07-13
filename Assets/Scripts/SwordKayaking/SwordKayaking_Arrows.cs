1e5df40b5eab57ea3dab9ff440a2a3a841d7167f
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-07-13 13:32:10 +1000

    Ayo you can play it... FOREVER!!!

--- Assets/Scripts/SwordKayaking/SwordKayaking_Arrows.cs
+++ Assets/Scripts/SwordKayaking/SwordKayaking_Arrows.cs
@@ -11,6 +11,36 @@ public class SwordKayaking_Arrows : MonoBehaviour
     void Start()
     {
         lifeTime = 1f;
+
+        // This randomly gives an arrow a direction when it spawns
+        int direction = Random.Range(0, 4);
+        switch (direction)
+        {
+            case 0:
+                print("UP");
+                this.transform.Rotate(0, 0, 0, Space.World);
+
+                input = "up";
+                break;
+            case 1:
+                print("LEFT");
+                this.transform.Rotate(0, 0, 90, Space.World);
+
+                input = "left";
+                break;
+            case 2:
+                print("RIGHT");
+                this.transform.Rotate(0, 0, -90, Space.World);
+
+                input = "right";
+                break;
+            case 3:
+                print("DOWN");
+                this.transform.Rotate(0, 0, 180, Space.World);
+
+                input = "down";
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -18,6 +48,8 @@ public class SwordKayaking_Arrows : MonoBehaviour
     {
         lifeTime -= Time.deltaTime;
 
+        // If the player hits the correct key they get a hit
+        // Otherwise if it is active too long or they hit the wrong key it's counted as a miss
         if (Input.GetKeyDown(input))
         {
             print("Key Hit");
--- Assets/Scripts/SwordKayaking/SwordKayaking_GameManager.cs
+++ Assets/Scripts/SwordKayaking/SwordKayaking_GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SwordKayaking_GameManager : MonoBehaviour
 {
+    float spawnTimer = 0.5f;
+
+    public GameObject arrowPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,15 @@ public class SwordKayaking_GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    // This just spawns arrows at a large enough delay they don't overlap their existance
     void Update()
     {
-        
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+        {
+            Instantiate(arrowPrefab, this.transform.position, Quaternion.identity);
+            spawnTimer = 1.1f;
+        }
     }
 }
