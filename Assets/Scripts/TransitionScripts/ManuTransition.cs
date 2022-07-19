b4b8abbfa064d33f7656a54a3b8506d140b3df7b
BanditZois <1026086@student.sae.edu.au>
2022-07-19 15:58:57 +1000

    Fixed transition from menu and the scene builds  :)

---
+++ Assets/Scripts/TransitionScripts/ManuTransition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManuTransition : MonoBehaviour
+{
+	
+	static List<int> LevelsToPlay = new List<int> { 0, 1, 2, 3, 4, 5 };
+	public int sceneRandomiser;
+	
+    // Start is called before the first frame update
+    public void Play()
+    {
+        int nextLevelIndex = Random.Range(0, LevelsToPlay.Count);
+        int nextLevel = LevelsToPlay[nextLevelIndex];
+        //This is SUPPOSED to remove the current level played from the available list
+        LevelsToPlay.Remove(nextLevel);
+        //Loads the next scene that was chosen from the before code
+		SceneManager.LoadScene(nextLevel);
+    }
+
+}
