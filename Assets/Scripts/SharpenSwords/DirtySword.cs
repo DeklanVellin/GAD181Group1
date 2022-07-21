bd5822a618e07b6cc2026e5ac3073b1caa228c38
Mareesoulis <1026214@student.sae.edu.au>
2022-07-21 15:19:54 +1000

    Win loss for cleaning sword

--- Assets/Scripts/SharpenSwords/DirtySword.cs
+++ Assets/Scripts/SharpenSwords/DirtySword.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class DirtySword : MonoBehaviour
 {
+    public float dateTimer = 4f;
     public int sharpCounter = 0;
+    public bool winCondition;
+    public SceneController gameComplete;
+    private int clean = 0;
+
+
     //collison thing on sword
     void OnCollisionExit(Collision collisionData)
     {
@@ -13,11 +19,43 @@ public class DirtySword : MonoBehaviour
             print("Don't touch me " + collisionData.gameObject.name);
             sharpCounter++;
             Destroy(this.gameObject);
+            clean++;
         }
-      
+
     }
 
-    //counter how many times it swipes
+
+    void Update()
+    {
+        dateTimer -= Time.deltaTime;
+
+        if (dateTimer <= 0)
+        {
+            gameComplete.WinScreen(false);
+        }
+
+        if (clean == 3)
+        {
+            winCondition = true;
+            WinLoss();
+        }
+        
+    }
+           
+    void WinLoss()
+    {
+        if (winCondition == true && dateTimer < 5)
+        {
+            //This loads the WIN scene
+            //SceneManager.LoadScene(5);
+            print("You've cleaned the sword!");
+            GameObject.FindObjectOfType<SceneController>().WinScreen(true);
+        }
+        
+    }
+}
+   
+//counter how many times it swipes
 
    /* if (Collision == gameObject)
        {
@@ -25,4 +63,3 @@ public class DirtySword : MonoBehaviour
         } */
 
 
-}
