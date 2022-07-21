f82d88ee81a59afd0d1fa033f51025a18ac9b1ee
Deklan Vellin <deklanvellin@hotmail.com>
2022-07-21 15:34:13 +1000

    Completed Fix to Mary's Game

--- Assets/Scripts/SharpenSwords/DirtySword.cs
+++ Assets/Scripts/SharpenSwords/DirtySword.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class DirtySword : MonoBehaviour
 {
-    public float dateTimer = 4f;
-    public int sharpCounter = 0;
-    public bool winCondition;
-    public SceneController gameComplete;
-    private int clean = 0;
+    void Start()
+    {
+        
+    }
 
 
     //collison thing on sword
@@ -17,42 +16,13 @@ public class DirtySword : MonoBehaviour
         if (collisionData.gameObject.name == "Hand")
         {
             print("Don't touch me " + collisionData.gameObject.name);
-            sharpCounter++;
+            
             Destroy(this.gameObject);
-            clean++;
+            GameObject.FindObjectOfType<IsSwordClean>().clean++;
         }
 
     }
 
-
-    void Update()
-    {
-        dateTimer -= Time.deltaTime;
-
-        if (dateTimer <= 0)
-        {
-            gameComplete.WinScreen(false);
-        }
-
-        if (clean == 3)
-        {
-            winCondition = true;
-            WinLoss();
-        }
-        
-    }
-           
-    void WinLoss()
-    {
-        if (winCondition == true && dateTimer < 5)
-        {
-            //This loads the WIN scene
-            //SceneManager.LoadScene(5);
-            print("You've cleaned the sword!");
-            GameObject.FindObjectOfType<SceneController>().WinScreen(true);
-        }
-        
-    }
 }
    
 //counter how many times it swipes
