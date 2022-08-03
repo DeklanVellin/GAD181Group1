8d2ac5af042f1597b2fb5e8aaac0bf6445019dc5
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-08-03 10:18:42 +1000

    Set up friut ninja after how long?

---
+++ Assets/Scripts/FuitNinja/FruitNinja_Spawners.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitNinja_Spawners : MonoBehaviour
+{
+    public GameObject[] fruits = new GameObject[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
