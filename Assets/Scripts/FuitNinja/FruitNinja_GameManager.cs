0294ccf1b54ba524e50431ec737f4b78665f4cf2
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-08-03 11:24:41 +1000

    Forgot game manager

---
+++ Assets/Scripts/FuitNinja/FruitNinja_GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Keep track of time and call wins and losses to scene controller
+public class FruitNinja_GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
